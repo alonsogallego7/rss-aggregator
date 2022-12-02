f9b762852aeadf294f750094e4dbcdcaaf3bf2b3
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2022-12-02 16:58:37 +0100

    bottom nav menu

--- app/src/main/java/com/alonsogallego/rss_aggregator/presentation/MainActivity.kt
+++ app/src/main/java/com/alonsogallego/rss_aggregator/presentation/MainActivity.kt
@@ -3,10 +3,29 @@ package com.alonsogallego.rss_aggregator.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alonsogallego.rss_aggregator.R
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.rss_feed -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.management -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.profile -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
