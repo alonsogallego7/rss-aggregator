786edf8f8e1df7d3b100cece0564ab045434e887
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2023-01-05 01:01:43 +0100

    get rss source use case

--- app/src/main/java/com/alonsogallego/rss_aggregator/data/SourceRssDataRepository.kt
+++ app/src/main/java/com/alonsogallego/rss_aggregator/data/SourceRssDataRepository.kt
@@ -1,6 +1,7 @@
 package com.alonsogallego.rss_aggregator.data
 
 import com.alonsogallego.rss_aggregator.data.local.SourceRssLocalDataSource
+import com.alonsogallego.rss_aggregator.domain.SourceRssModel
 import com.alonsogallego.rss_aggregator.domain.SourceRssRepository
 
 class SourceRssDataRepository(private val localSource: SourceRssLocalDataSource):
@@ -8,6 +9,10 @@ class SourceRssDataRepository(private val localSource: SourceRssLocalDataSource)
 
     override suspend fun create(webName: String, url: String) {
         localSource.create(webName, url)
+    }
+
+    override suspend fun getAll(): List<SourceRssModel> {
+        TODO("Not yet implemented")
     }
 
 }
